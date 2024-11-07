0054b5d36cb09620797dbf253c1f8a4a04a3759c
DevLabsGT <devlabsgt@gmail.com>
2024-11-07 16:01:59 -0600

    ultimos cambios de ususuarrio

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ const { router: usuarioRoutes } = require("../controllers/usuariosController"); 
 const proyectoRoutes = require("../controllers/proyectoController"); // Rutas de proyectos
 const beneficiarioRoutes = require("../controllers/beneficiarioController"); // Rutas de beneficiarios
 const donanteRoutes = require("../controllers/donanteController"); // Rutas de donantes
-
+const {
+  router: mailConfigRoutes,
+} = require("../controllers/mailConfigController"); // Rutas de donantes
 
 // Usa las rutas importadas sin prefijos
 router.use(usuarioRoutes);
 router.use(proyectoRoutes);
 router.use(beneficiarioRoutes);
 router.use(donanteRoutes);
-
+router.use(mailConfigRoutes);
 // Exporta el router unificado
 module.exports = router;
